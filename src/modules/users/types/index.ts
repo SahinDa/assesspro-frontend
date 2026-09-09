1a42f0eb4331803402372253c5813c1f99958cdd
null <null>
2026-09-09 23:46:06 +0530

    added user interface

---
+++ src/modules/users/types/index.ts
@@ -0,0 +1,22 @@
+import { UserRoleType } from '@/types/enums';
+
+export interface User {
+  user_id: string;
+  firstname: string;
+  lastname: string;
+  email: string;
+  oauth_provider: number | null;
+  oauth_id: string | null;
+  email_verified: boolean;
+  role: UserRoleType;
+  status: number;
+  profile_pic: string | null;
+  created_at: string;
+  updated_at: string;
+}
+
+export interface UpdateUserProfileDto {
+  firstname?: string;
+  lastname?: string;
+  profile_pic?: string;
+}
